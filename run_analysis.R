4679f0364ee35de0d144ba3702276f124763d6de
jeffz2 <jeffreyzhou18@gmail.com>
2022-06-01 14:50:12 -0400

    implemented labels for activities and column names

--- run_analysis.R
+++ run_analysis.R
@@ -16,8 +16,21 @@ run_analysis <- function () {
   colnames(features) <- featureNames[[2]]
   colnames(activity) <- "Activity"
   colnames(subject) <- "Subject"
-  data <- cbind(features,activity, subject)
-  
-  labelIndices <- grep("mean|std", featureNames[[2]])
-  data <- data[labelIndices]
+  for (i in 1:6) {
+    activity[activity == i] <- activityLabels[i, 2]
+  }
+  colnames(features) <- sub("Acc", "Accelerometer", colnames(features))
+  colnames(features) <- sub("^t", "Time", colnames(features))
+  colnames(features) <- sub("^f", "Frequency", colnames(features))
+  colnames(features) <- sub("Gyro", "Gyroscope", colnames(features))
+  colnames(features) <- sub("Mag", "Magnitude", colnames(features))
+  colnames(features) <- sub("BodyBody", "Body", colnames(features))
+
+  d <- cbind(features,activity, subject)
+
+  labelIndices <- grep("mean|std|Activity|Subject", colnames(d))
+  d <- d[labelIndices]
 }
+  
+  
+
